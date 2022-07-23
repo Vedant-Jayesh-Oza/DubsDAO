e3cbb33d2978bd33aed88411a7c07ec0da516c33
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2022-07-23 10:31:51 +0530

    voting frontend and revoking token minting rights

---
+++ scripts/10-create-vote-proposals.js
@@ -0,0 +1,68 @@
+import sdk from "./1-initialize-sdk.js";
+import { ethers } from "ethers";
+
+// This is our governance contract.
+const vote = sdk.getVote("0xCF00bCC3C72d1517AE143Cc7FF6b300239Df0DEF");
+
+// This is our ERC-20 contract.
+const token = sdk.getToken("0x7b62e6137EB155A65c83b02aD6bD1A3Bab9AdF5b");
+
+(async () => {
+  try {
+    // Create proposal to mint 420,000 new token to the treasury.
+    const amount = 420_000;
+    const description = "Should the DAO mint an additional " + amount + " tokens into the treasury?";
+    const executions = [
+      {
+        
+        toAddress: token.getAddress(),
+        
+        nativeTokenValue: 0,
+        
+        transactionData: token.encoder.encode(
+          "mintTo", [
+          vote.getAddress(),
+          ethers.utils.parseUnits(amount.toString(), 18),
+        ]
+        ),
+      }
+    ];
+
+    await vote.propose(description, executions);
+
+    console.log("✅ Successfully created proposal to mint tokens");
+  } catch (error) {
+    console.error("failed to create first proposal", error);
+    process.exit(1);
+  }
+
+  try {
+    // Create proposal to transfer ourselves 6,900 tokens for being awesome.
+    const amount = 6_900;
+    const description = "Should the DAO transfer " + amount + " tokens from the treasury to " +
+      process.env.WALLET_ADDRESS + " for being awesome?";
+    const executions = [
+      {
+        // Again, we're sending ourselves 0 ETH. Just sending our own token.
+        nativeTokenValue: 0,
+        transactionData: token.encoder.encode(
+          // We're doing a transfer from the treasury to our wallet.
+          "transfer",
+          [
+            process.env.WALLET_ADDRESS,
+            ethers.utils.parseUnits(amount.toString(), 18),
+          ]
+        ),
+        toAddress: token.getAddress(),
+      },
+    ];
+
+    await vote.propose(description, executions);
+
+    console.log(
+      "✅ Successfully created proposal to reward ourselves from the treasury, let's hope people vote for it!"
+    );
+  } catch (error) {
+    console.error("failed to create second proposal", error);
+  }
+})();
---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const token = sdk.getToken("0x7b62e6137EB155A65c83b02aD6bD1A3Bab9AdF5b");
+
+(async () => {
+  try {
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
